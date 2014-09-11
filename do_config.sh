87436dd8992b8eeb1e39fa6f10a653f3c3814722
John Archie McKown <john.archie.mckown@gmail.com>
2014-09-11 07:28:45 -0500

    I added the do_config.sh file. This basically just issues the ./configure command will all the options that I want. For me, this is the easiest way to change those options and redo the configure command.
    The contents of the file are the ./configure that I used to generate
    the BASH makefile et al. on a z/OS V2R1 system.

---
+++ do_config.sh
@@ -0,0 +1,6 @@
+./configure --enable-alias --enable-arith-for-command --enable-array-variables --enable-brace-expansion --enable-casemod-attributes --enable-casemod-expansions \
+--enable-cond-command --enable-cond-regexp --enable-coprocesses --enable-directory-stack --enable-disabled-builtins \
+--enable-dparen-arithmetic --enable-extended-glob --enable-help-builtin --enable-history --enable-job-control --enable-process-substitution \
+--enable-progcomp --enable-readline --enable-select --with-curses \
+CC=xlc CFLAGS='-Wc,TARG(LE,ZOSV1R12),AGG,SKIPSRC(HIDE),XREF,ARCH(5),TUNE(7),SOURCE,SHOWMAC,EXPMAC,SHOWINC,LANGLVL(LONGLONG) -O2'      
+#C=xlc CFLAGS='-Wc,AGG,SKIPSRC(HIDE),XREF,ARCH(5),TUNE(7),SOURCE,SHOWMAC,EXPMAC,SHOWINC,LANGLVL(LONGLONG) -O2'                        
