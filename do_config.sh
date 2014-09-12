551e7756800c088ef63adbed4558f6475b166fe5
John Archie McKown <john.archie.mckown@gmail.com>
2014-09-11 19:10:51 -0500

    do_config.* files are z/OS files to help do the ./configure command

--- do_config.sh
+++ do_config.sh
@@ -2,5 +2,6 @@
 --enable-cond-command --enable-cond-regexp --enable-coprocesses --enable-directory-stack --enable-disabled-builtins \
 --enable-dparen-arithmetic --enable-extended-glob --enable-help-builtin --enable-history --enable-job-control --enable-process-substitution \
 --enable-progcomp --enable-readline --enable-select --with-curses \
-CC=xlc CFLAGS='-Wc,TARG(LE,ZOSV1R12),AGG,SKIPSRC(HIDE),XREF,ARCH(5),TUNE(7),SOURCE,SHOWMAC,EXPMAC,SHOWINC,LANGLVL(LONGLONG) -O2'      
-#C=xlc CFLAGS='-Wc,AGG,SKIPSRC(HIDE),XREF,ARCH(5),TUNE(7),SOURCE,SHOWMAC,EXPMAC,SHOWINC,LANGLVL(LONGLONG) -O2'                        
+CC=xlc CFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_XOPEN_SOURCE=600 -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2'
+       CPPFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_XOPEN_SOURCE=600 -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2'
+#C=xlc CFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -qagg -qsource -qshowmacros -qshow -qexpmac -D_ALL_SOURCE -O2'
