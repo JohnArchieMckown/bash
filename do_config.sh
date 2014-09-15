7c5a0f5a7e799a1bb25628c98a432629b2d291f3
John Archie McKown <john.archie.mckown@gmail.com>
2014-09-15 12:56:52 -0500

    I added the --without-bash-malloc to the ./configure parameters. For some reason, using the builting BASH malloc caused memory problems. I don't know why, and I don't really know how to debug them. But this appears to work.
    This is the first commit where the executable actually seems to
    work properly. Of course, I do to more testing.

--- do_config.sh
+++ do_config.sh
@@ -2,6 +2,8 @@
 --enable-cond-command --enable-cond-regexp --enable-coprocesses --enable-directory-stack --enable-disabled-builtins \
 --enable-dparen-arithmetic --enable-extended-glob --enable-help-builtin --enable-history --enable-job-control --enable-process-substitution \
 --enable-progcomp --enable-readline --enable-select --with-curses \
-CC=xlc CFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_XOPEN_SOURCE=600 -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2' \
-       CPPFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_XOPEN_SOURCE=600 -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2'
+--without-bash-malloc \
+--prefix=/u/joarmc/projects/bash/dist \
+CC=xlc CFLAGS='-g -qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2' \
+       CPPFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2'
 #C=xlc CFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -qagg -qsource -qshowmacros -qshow -qexpmac -D_ALL_SOURCE -O2'
