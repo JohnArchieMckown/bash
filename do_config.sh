2fb65a7b6d17b37c8f4f499bee326c6f1d11dc37
John Archie McKown <john.archie.mckown@gmail.com>
2014-09-16 08:51:23 -0500

    Minor reformatting to separate parameters onto separate lines.

--- do_config.sh
+++ do_config.sh
@@ -1,8 +1,27 @@
-./configure --enable-alias --enable-arith-for-command --enable-array-variables --enable-brace-expansion --enable-casemod-attributes --enable-casemod-expansions \
---enable-cond-command --enable-cond-regexp --enable-coprocesses --enable-directory-stack --enable-disabled-builtins \
---enable-dparen-arithmetic --enable-extended-glob --enable-help-builtin --enable-history --enable-job-control --enable-process-substitution \
---enable-progcomp --enable-readline --enable-select --with-curses \
+./configure --enable-alias \
+--enable-arith-for-command \
+--enable-array-variables \
+--enable-brace-expansion \
+--enable-casemod-attributes \
+--enable-casemod-expansions \
+--enable-cond-command \
+--enable-cond-regexp \
+--enable-coprocesses \
+--enable-directory-stack \
+--enable-disabled-builtins \
+--enable-dparen-arithmetic \
+--enable-extended-glob \
+--enable-help-builtin \
+--enable-history \
+--enable-job-control \
+--enable-process-substitution \
+--enable-progcomp \
+--enable-readline \
+--enable-select \
+--with-curses \
 --without-bash-malloc \
+--disable-mem-scramble \
+--without-libintl-prefix \
 --prefix=/u/joarmc/projects/bash/dist \
 CC=xlc CFLAGS='-g -qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2' \
        CPPFLAGS='-qtarg=zosv1r12 -qskipsrc=hide -qarch=5 -qtune=7 -qlanglvl=extended:longlong -D_LARGE_FILES -D_ALL_SOURCE -D_ISOC99_SOURCE -O2'
--- do_make.jcl
+++ do_make.jcl
@@ -1,9 +1,12 @@
-//BASHCMPL JOB CLASS=K,MSGCLASS=H,NOTIFY=&SYSUID                                
+//BASHCMPL JOB CLASS=K,                                                         
+//         MSGCLASS=H,
+//         NOTIFY=&SYSUID,
+//         TIME=NOLIMIT
 //BPXBATCH EXEC PGM=BPXBATCH,REGION=0M                                          
-//STDOUT DD SYSOUT=*                                                            
-//STDIN DD PATH='/dev/null',PATHOPTS=(ORDONLY)                                  
-//STDERR DD SYSOUT=*                                                            
-//STDPARM DD *                                                                  
-SH cd /u/joarmc/projects/bash;make                                              
+//STDOUT   DD   SYSOUT=*                                                        
+//STDIN    DD   PATH='/dev/null',PATHOPTS=(ORDONLY)                             
+//STDERR   DD   SYSOUT=*                                                        
+//STDPARM  DD   *                                                               
+SH cd /u/joarmc/projects/bash && make clean && make                             
 /*                                                                              
 //                                                                              
